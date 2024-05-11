9c744ff360d6041e109fc840037fcc016b4d8a77
Matheus Sena <mpwsenadev@gmail.com>
2024-05-10 22:33:38 -0300

    Add files via upload

---
+++ Comparacao.java
@@ -0,0 +1,11 @@
+package basico;
+
+public class Comparacao {
+
+	public static void main(String[] args) {
+		boolean resultado = 10==10;	// == =! >= > < <=
+		System.out.println(resultado); // true - false
+		
+	}
+
+}
