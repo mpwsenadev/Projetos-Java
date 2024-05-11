4e515d2dd9692ddb2b111a27073888acd050f359
Matheus Sena <mpwsenadev@gmail.com>
2024-05-10 22:36:32 -0300

    Add files via upload

---
+++ EstruturaFor.java
@@ -0,0 +1,13 @@
+package basico;
+
+public class EstruturaFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<10; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
