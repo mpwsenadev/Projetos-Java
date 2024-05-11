f75d7115741d67b5775574902f33df7753d2091c
Matheus Sena <mpwsenadev@gmail.com>
2024-05-10 22:31:28 -0300

    Add files via upload

---
+++ OlaMundo.java
@@ -0,0 +1,14 @@
+package basico;
+
+public class OlaMundo {
+	public static void main(String[] args) {
+		/*
+		 * Comentário
+		 * de
+		 * várias
+		 * linhas
+		 */
+		System.out.println("Olá Mundo");
+		// Esse código imprime Olá Mundo
+	}
+}
