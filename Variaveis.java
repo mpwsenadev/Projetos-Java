3be21db29513cc449502cce1d7792b503c8a3207
Matheus Sena <mpwsenadev@gmail.com>
2024-05-10 22:32:00 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,15 @@
+package basico;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		//tipo nome = conteúdo;
+		String meuNome = "Matheus Sena";
+		int idade = 20;
+		double altura = 1.72;
+		char sexo = 'M';
+		boolean casado = false;
+		
+		System.out.println("Meu nome é: "+meuNome+" e minha idade é "+idade);
+	}
+}
