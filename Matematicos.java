4912cc1e0eefc2864354e802d0cfa9ee329a09f1
Matheus Sena <mpwsenadev@gmail.com>
2024-05-10 22:32:36 -0300

    Add files via upload

---
+++ Matematicos.java
@@ -0,0 +1,25 @@
+package basico;
+
+public class Matematicos {
+
+    public static void main(String[] args) {
+    	
+    	//println != print
+    	
+    	// Soma
+        int resultado1 = 10 + 3;
+        System.out.println(resultado1);
+        // Subtração
+        int resultado2 = 10 - 3;
+        System.out.println(resultado2);
+        // Divisão
+        int resultado3 = 10 / 3;
+        System.out.println(resultado3);
+        // Multiplicação
+        int resultado4 = 10 * 3;
+        System.out.println(resultado4);
+        // Resto da divisão
+        int resultado5 = 10 % 3;
+        System.out.println(resultado5);
+    }
+}
