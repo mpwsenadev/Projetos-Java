3e6ddc42abf8133bda42327b3b7f2d36c1973f74
Matheus Sena <mpwsenadev@gmail.com>
2024-05-10 22:35:52 -0300

    Add files via upload

---
+++ FacaEnquanto.java
@@ -0,0 +1,14 @@
+package basico;
+
+public class FacaEnquanto {
+
+	public static void main(String[] args) {
+		int numero = 1;
+		do {
+			System.out.println(numero);
+			numero++;
+		} while(numero<10);
+
+	}
+
+}
