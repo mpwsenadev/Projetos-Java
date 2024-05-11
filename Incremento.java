f8aed30501770066d88b086f9ce7f48b16003bfa
Matheus Sena <mpwsenadev@gmail.com>
2024-05-10 22:33:09 -0300

    Add files via upload

---
+++ Incremento.java
@@ -0,0 +1,13 @@
+package basico;
+
+public class Incremento {
+
+	public static void main(String[] args) {
+		int numero = 1;
+		numero++; // Será incrementado na próxima linha de execução
+		System.out.println(numero); // System.out.println(++numero) -->> Será incremetado na própria linha de execução
+		//Decremento (--numero) -->> Decrementa depois atribui o valor
+		//Decremento (numero--) -->> Atribui o valor depois decrementa
+	}
+
+}
